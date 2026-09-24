f121f1f6e81c052eb983d7674692c440eeae2a64
Choi KyeongSeok <118911096+Sentiaix@users.noreply.github.com>
2026-09-24 04:49:54 +0000

    .

---
+++ test.c
@@ -0,0 +1,14 @@
+// https://level.goorm.io/exam/352707/abcd-%EC%A0%95%EB%A0%AC/quiz/1
+
+#include <stdio.h>
+
+int main(){
+    int a;
+
+    scanf("%d", &a);
+    printf("The value is : %d\n", a);
+
+    printf("hello world!\n");
+
+    return 0;
+}
