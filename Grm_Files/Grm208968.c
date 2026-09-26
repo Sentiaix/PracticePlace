2170a2314b949724b957e49f708610f0e1b799b4
Choi KyeongSeok <118911096+Sentiaix@users.noreply.github.com>
2026-09-26 09:42:27 +0000

    .

--- Grm_Files/Grm208968.c
+++ Grm_Files/Grm208968.c
@@ -5,6 +5,10 @@
 
 #define MAX_SIZE 40
 
+// Best(N) > 입력에 N이 주어지므로 N을 소인수분해함.
+// 큰 순서대로 정렬하면 가장 큰 숫자가 탄생.
+// Best(M) > ??
+
 // qsort를 사용하여 글자 대조
 //     int compare(const void* a, const void* b){
 //     int x = *(const int*)a;
@@ -19,23 +23,30 @@
 //     return strcmp(yx, xy);
 // }
 
-int factorize(long long* arr, long long n);
+void factorize(long long* arr, long long n);
+void bestM(long long* arr, long long n);
 void solve();
 
 int main(){
-	long long num; // 2-10e11
+	long long num; // 2-10e11, 성현이의 숫자
 	if (scanf("%lld", &num) != 1) return 0;
 
 	// int size = (int)sqrt(num); // 10^12의 최대 소인수 갯수는 39개임.
-	long long arr[MAX_SIZE];
+	long long arr_n[MAX_SIZE];
+	long long arr_m[MAX_SIZE];
 
-	factorize(arr, num);
+	// factorize(arr, num);
 
 
 	return 0;
 }
 
-int factorize(long long* arr, long long n){
+void factorize(long long* arr, long long n){
+	if (n == 2) {
+		arr[0] = 2;
+		return;
+	}
+
 	int idx = 0;
 	for (long long d = 2; d <= n / d; d++){
 		while(n % d == 0){
@@ -48,6 +59,13 @@ int factorize(long long* arr, long long n){
 	// for (int i = 0; i < idx; i++){
 	// 	printf("%lld", arr[i]);
 	// }
+}
+
+void bestM(long long* arr, long long n){
+	long long m = 1; // min(N) == 2, min(m) == 1
+	for (int i = 0; i < n; i++){
+		
+	}
 }
 
 void solve(){
